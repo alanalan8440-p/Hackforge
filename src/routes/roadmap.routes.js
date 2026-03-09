388fd448b3416b83a87fb724e7c82cbc3007bda6
Alan <alan8440@gmail.com>
2026-03-09 16:45:57 +0530

    fix routes import syntax

--- src/routes/roadmap.routes.js
+++ src/routes/roadmap.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { roadmap } from "../controllers/roadmap.controller.js";
+import roadmap from "../controllers/roadmap.controller.js";
 
 const router = express.Router();
 
--- src/services/match.service.js
+++ src/services/match.service.js
@@ -1,4 +1,4 @@
-import { supabase } from "../config/supabase.js";
+import supabase from "../config/supabase.js";
 import { calculateScore } from "../utils/scoring.js";
 
 export const matchCourses = async (user) => {
